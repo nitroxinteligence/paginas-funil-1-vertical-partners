6e9e11133df6845d6b6b66219099b33fc68241de
Mateus M <nitroxinteligence@gmail.com>
2026-01-13 15:13:47 -0300

    Fix fetch rewrite under /lp-funil and bust iframe cache

--- lps-funis-vp/src/app/lp-funil/page.tsx
+++ lps-funis-vp/src/app/lp-funil/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
     <iframe
-      src="/lp-funil/index.html?v=920d489"
+      src="/lp-funil/index.html?v=cd1532d"
       title="LP Funil"
       style={{ width: "100%", height: "100vh", border: "0" }}
     />
